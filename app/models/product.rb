a49302337825c2f91cc400833f791523e5ae27d2
Alan Ren <alanlin786@gmail.com>
2024-04-21 18:33:46 -0500

    adding the javascript/css

---
+++ app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ApplicationRecord
+    #validate :name, presence: true, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create( name: 'Eco Friendly Reusable Water Bottle', 
+                price: 14.99, 
+                description: 'Stay hydrated and environmentally conscious with our 24 oz BPA-free, durable, and reusable water bottle. Featuring a leak-proof cap and a convenient carry handle, it’s perfect for on-the-go lifestyles and reduces plastic waste.')
+
+Product.create( name: 'Bluetooth Wireless Earbuds', 
+                price: 59.95, 
+                description: 'Experience the freedom of true wireless listening with these sleek Bluetooth earbuds. Offering high-quality sound, easy connectivity, and a comfortable fit, they are perfect for your daily commute, gym sessions, or enjoying music on the go.')
+
+Product.create( name: 'Gourmet Arabica Coffee Beans', 
+                price: 12.50, 
+                description: 'Our gourmet Arabica coffee beans are sourced from the highlands of Colombia, offering a smooth, rich, and full-bodied flavor. Perfect for coffee connoisseurs looking to brew a superior cup at home.')
+
+
+Product.create( name: 'Organic Dog Treats', 
+                price: 8.99, 
+                description: 'Treat your furry friend to the best with our organic, gluten-free dog treats made from all-natural ingredients. These treats are not only tasty but also provide essential nutrients to keep your pet healthy and happy.')
+
+Product.create( name: ' LED Desk Lamp with Wireless Charger', 
+                price: 45.00, 
+                description: 'Illuminate your workspace with our stylish LED desk lamp that also doubles as a wireless charger. Featuring adjustable brightness levels and a flexible arm, this lamp combines functionality with modern design.')
